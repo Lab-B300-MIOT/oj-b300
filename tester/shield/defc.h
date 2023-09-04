c4df45840dab9374bc0dbd224cf7fce8c16f83fc
zer0-911 <mohiqbalf5@gmail.com>
2023-09-04 13:01:22 +0700

    first commit

---
+++ tester/shield/defc.h
@@ -0,0 +1,35 @@
+/*
+
+@file defc.h
+There should be a newline at end of this file.
+Put the message displayed to user after // in each line
+
+e.g. If you want to disallow goto, add this line:
+#define goto errorNo13    //Goto is not allowd
+
+*/
+
+#define system errorNo1      //"system" is not allowed
+#define freopen errorNo2     //File operation is not allowed
+#define fopen errorNo3       //File operation is not allowed
+#define fprintf errorNo4     //File operation is not allowed
+#define fscanf errorNo5      //File operation is not allowed
+#define feof errorNo6        //File operation is not allowed
+#define fclose errorNo7      //File operation is not allowed
+#define ifstream errorNo8    //File operation is not allowed
+#define ofstream errorNo9    //File operation is not allowed
+#define fork errorNo10       //Fork is not allowed
+#define clone errorNo11      //Clone is not allowed
+#define sleep errorNo12      //Sleep is not allowed
+#define execlp errorNo13      //Execlp is not allowed
+#define execvp errorNo14      //Execvp is not allowed
+#define execvpe errorNo15     //Execvpe is not allowed
+#define execlp errorNo16      //Execlp is not allowed
+#define execl errorNo17      //Execl is not allowed
+#define execle errorNo18      //Execle is not allowed
+#define execv errorNo19      //Execv is not allowed
+#define socket errorNo20      //socket is not allowed
+#define bind errorNo21      //socket is not allowed
+#define listen errorNo22      //socket is not allowed
+#define accept errorNo23      //socket is not allowed
+#define connect errorNo24      //socket is not allowed
